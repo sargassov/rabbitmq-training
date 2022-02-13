d852e4da1fa5493de5dbf254303b135846b07ecb
sargassov <sargassov@yandex.ru>
2022-02-14 00:16:51 +0300

    .

--- src/main/java/com/rabbitmq/trainig/source/publicists/Publicist.java
+++ src/main/java/com/rabbitmq/trainig/source/publicists/Publicist.java
@@ -4,7 +4,6 @@ import com.rabbitmq.client.BuiltinExchangeType;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-
 import java.util.Scanner;
 
 public class Publicist {
